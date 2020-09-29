82e6a71e95d089f442ae4e8e2cc98fd057612d73
kaisei <kaisei101010@icloud.com>
2020-09-29 22:41:43 +0900

    htmlcs

---
+++ index.js
@@ -0,0 +1,9 @@
+const question = 'けんすけのお姉ちゃんの名前はどれ？';
+const anwers =['りこ','まみこ','れな','ゆうき'];
+const currect = 'ゆうき';
+
+const button = document.getElementsByTagName('button')
+
+document.getElementById('js-text').textContent = question;
+let buttonIndex = 0;
+let buttonLength
